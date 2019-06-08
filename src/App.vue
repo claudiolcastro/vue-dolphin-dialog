5a6b3956cf90d7286e927b3b270a3de04a6fa07f
Claudio Castro <claudioluiz.castro@gmail.com>
2019-06-08 18:58:28 -0300

    First Draft dolphin menu

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,41 @@
 <template>
   <div id="app">
+    <button @click="show = !show">Open Menu</button>
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <DolphinMenu :class="{show: show, hide: !show}"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import DolphinMenu from './components/DolphinMenu.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
+    DolphinMenu,
+  },
+  data() {
+    return {
+      show: false,
+    }
   },
 };
 </script>
 
 <style lang="scss">
+body {
+  margin: 0;
+}
+
+button {
+  border: 1px solid #ccc;
+}
+
+img {
+  display: block;
+  margin: 0 auto;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +43,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.show {
+  bottom: 0 !important;
+}
+.hide {
+  bottom: -50% !important;
 }
 </style>
---
+++ src/components/DolphinMenu.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="dolphin-menu">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DolphinMenu',
+};
+</script>
+
+<style scoped lang="scss">
+.dolphin-menu {
+  background-color: #ccc;
+  border-radius: 10px 10px 0 0;
+  height: 50vh;
+  transition: .5s;
+  position: absolute;
+  width: 100%;
+  z-index: 1;
+}
+
+</style>
